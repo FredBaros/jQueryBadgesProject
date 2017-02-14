b779fc5f48e78fe4a8a5efcbd7197818f886f0bd
FredBaros <fsb877@gmail.com>
2017-02-14 12:22:54 +0000

    Inserted the code as requested.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,32 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/FredericoB.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      // handle response
+      populateWithCourses(data.courses.completed)
+    }
+  });
+  function populateWithCourses(courses){
+    var $badges = $('#badges');
+    courses.forEach(function(course){
+      $div = $('<div />',{
+        'class':'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+    });
+  }
 
 });
